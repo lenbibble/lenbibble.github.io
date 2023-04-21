c0920bb06b4907a4b493cda838eb820260a9bd43
lenbibble <lenbibble@outlook.com>
2023-04-21 20:57:37 +0800

    Change

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-// calculate the time elapsed since the last update
+// make document update every 1000ms and record the time of last update
 var lastUpdate = new Date().getTime();
+setInterval(function() {
+    lastUpdate = new Date().getTime();
+}, 1000);
+
 var time = new Date().getTime() - lastUpdate;
 console.log(time);
-// make the button elemnts clickable and showing the time elapsed in elements with class "Time_show"
-var buttons = document.getElementsByClassName("button");
-for (var i = 0; i < buttons.length; i++) {
-  buttons[i].onclick = function() {
-    var time_show = document.getElementsByClassName("Time_show");
-    for (var j = 0; j < time_show.length; j++) {
-      time_show[j].innerHTML = time;
-    }
-  }
-}
+// make the button with id 'show_time' clickable and showing the time elapsed in elements with id "Time_show"
+document.getElementById("show_time").onclick = function() {
+    document.getElementById("Time_show").innerHTML = time;
+};
