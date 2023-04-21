db545365396a452f937751f1f3488e8a006fdc27
lenbibble <lenbibble@outlook.com>
2023-04-21 20:38:47 +0800

    changeScript

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // calculate the time elapsed since the last update
+var lastUpdate = new Date().getTime();
 var time = new Date().getTime() - lastUpdate;
 console.log(time);
 // make the button elemnts clickable and showing the time elapsed in elements with class "Time_show"
