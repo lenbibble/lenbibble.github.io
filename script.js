f96e9c3bf40eb60aa05aeb153083cfa449115e8a
lenbibble <lenbibble@outlook.com>
2023-04-21 20:09:50 +0800

    randomChange

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // calculate the time elapsed since the last update
 var time = new Date().getTime() - lastUpdate;
-// make the button elemnts clickable and showing the time elapsed
+// make the button elemnts clickable and showing the time elapsed in elements with class "Time_show"
 var buttons = document.getElementsByClassName("button");
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].onclick = function() {
-    this.innerHTML = time + " ms";
+    var time_show = document.getElementsByClassName("Time_show");
+    for (var j = 0; j < time_show.length; j++) {
+      time_show[j].innerHTML = time;
+    }
   }
 }
